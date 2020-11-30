96332ed37a0d2b93933ecfb246c681adf6d73181
Santigie Sankoh <santigiesankoh233@gmail.com>
2020-12-01 00:33:29 +0200

    updated

--- Assignment Week 8/Food_test.py
+++ Assignment Week 8/Food_test.py
@@ -2,16 +2,30 @@ import unittest
 
 
 
-class Food(unittest.TestCase):
-    def randomize_food_position(self):
-        self.assertEqual([0], [0])
+class TestFood(unittest.TestCase):
+    def test_screen_width(self):
+        """
+        This unittest test for game screen width. We do this by passing values and testing for equality.
+        """
+        for y in range(0, 8):
+            with self.subTest(y=y):
+                self.assertEqual((y + y) % 2, 0)
 
-    def draw_food(self):
-        self.assertEqual((12, 2, 4), (12, 2, 4))
+    def test_screen_height(self):
+        """
+        This unittest test for game screen height. We do this by passing values and testing for equality.
+        """
+        for y in range(0, 8):
+            with self.subTest(y=y):
+                self.assertEqual((y + y) % 2, 0)
 
-    def My_font(self):
-        self.assertEqual('Monospace', 'Monospace')
+    def test_color(self):
+        """
+        This unittest test for food color. We do this by passing values and testing for equality.
+        """
+        result = self.color = (223, 163, 49)
+        self.assertEqual(result, (223, 163, 49))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main( )
--- Assignment Week 8/Snake_test.py
+++ Assignment Week 8/Snake_test.py
@@ -1,4 +1,5 @@
 import unittest
+import Snake_class
 
 positions = []
 
@@ -7,24 +8,26 @@ def get_head_position():
     pass
 
 
-class Snake(unittest.TestCase):
-    def exit_game(self):
+class Test_snake(unittest.TestCase):
+
+    def test_exit(self):
         self.assertEqual('exit'.upper( ), 'EXIT')
 
-    def handle_keys(self):
+    def test_handle_keys(self):
         from pygame.examples.prevent_display_stretching import event
         import pygame
         self.assertIs(event.key, pygame.K_DOWN)
 
-    def reset(self):
-        snake_position = [0]
+    def test_reset(self):
+        snake_position = 0
         self.assertIs(snake_position, 0)
 
-    def move(self):
-        self.assertIn(get_head_position( ), positions.pop( ))
+    def test_move(self):
+        positions.pop()
+        self.assertIn(get_head_position(), positions.pop())
 
-    def draw(self):
-        self.assertEqual([0, 1, 2, 3], [1, 2, 3])
+    def test_draw(self):
+        self.assertEqual([0, 1, 2, 3], [0, 1, 2, 3])
 
 
 if __name__ == '__main__':
